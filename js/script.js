ff3421b8596301e2dd03747ffe00a73eabc21564
Pradeep Dhungana <100480407+dhunganaPradeep@users.noreply.github.com>
2022-08-05 01:34:09 +0545

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Typing animation
+
+var typed = new Typed(".typing",{
+    strings:["Student","Youtuber","Web Designer","Graphics Designer"],
+    typeSpeed:100,
+    Backspeed:60,
+    loop:true
+})
